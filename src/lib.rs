a8d8a9324d0ee1d7ada6b1f627202a805ea7ce2d
Jörn Horstmann <git@jhorstmann.net>
2022-11-30 22:30:34 +0100

    Project setup

---
+++ src/lib.rs
@@ -0,0 +1,78 @@
+use std::array::TryFromSliceError;
+use std::fmt::{Display, Formatter};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+use std::str::Utf8Error;
+
+#[derive(Debug)]
+pub enum Error {
+    Io(std::io::Error),
+    ParseInt(std::num::ParseIntError),
+    ParseFloat(std::num::ParseFloatError),
+    ParseUtf8(Utf8Error),
+    General(String),
+    PatternMatch,
+    EmptyIterator,
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Self {
+        Error::Io(e)
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(e: std::num::ParseIntError) -> Self {
+        Error::ParseInt(e)
+    }
+}
+
+impl From<std::num::ParseFloatError> for Error {
+    fn from(e: std::num::ParseFloatError) -> Self {
+        Error::ParseFloat(e)
+    }
+}
+
+impl From<Utf8Error> for Error {
+    fn from(e: Utf8Error) -> Self {
+        Error::ParseUtf8(e)
+    }
+}
+
+impl From<std::convert::Infallible> for Error {
+    fn from(_e: std::convert::Infallible) -> Self {
+        panic!("Infallible error should never occur")
+    }
+}
+
+impl From<TryFromSliceError> for Error {
+    fn from(e: TryFromSliceError) -> Self {
+        Error::General(e.to_string())
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::Io(e) => f.write_fmt(format_args!("Io: {}", e)),
+            Error::ParseInt(e) => f.write_fmt(format_args!("Parse: {}", e)),
+            Error::ParseFloat(e) => f.write_fmt(format_args!("Parse: {}", e)),
+            Error::ParseUtf8(e) => f.write_fmt(format_args!("Parse: {}", e)),
+            Error::General(s) => f.write_fmt(format_args!("General: {}", s)),
+            Error::PatternMatch => f.write_str("Pattern mismatch"),
+            Error::EmptyIterator => f.write_str("Empty iterator"),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+pub fn read_lines(file: &str) -> Result<Vec<String>> {
+    let path = Path::new(file);
+    let io = File::open(path)?;
+    let br = BufReader::new(io);
+    Ok(br.lines().collect::<std::io::Result<Vec<String>>>()?)
+}
