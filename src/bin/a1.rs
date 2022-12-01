ce148e3a7689a175204a7baf04952d54b1428b4a
Jörn Horstmann <git@jhorstmann.net>
2022-12-01 21:28:15 +0100

    Day 1

--- src/bin/a1.rs
+++ src/bin/a1.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use adventofcode2022::read_lines;
+use adventofcode2022::Result;
+
+fn main() -> Result<()> {
+    let lines = read_lines("data/a1.txt")?;
+    let chunks = lines.split(String::is_empty).collect::<Vec<_>>();
+    let mut sums = chunks
+        .iter()
+        .map(|chunk| chunk.iter().map(|x: &String| x.parse::<i32>().unwrap()).sum::<i32>())
+        .collect::<Vec<_>>();
+    let max = sums.iter().copied().max().unwrap_or_default();
+
+    println!("part1: {max}");
+
+    sums.sort();
+    sums.reverse();
+
+    let max3 = sums.iter().copied().take(3).sum::<i32>();
+    println!("part2: {max3}");
+
+    Ok(())
 }
