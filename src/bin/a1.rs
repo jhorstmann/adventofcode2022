5fbf03db8accd7f9d0ff012cfbd00c695b851e5c
Joern Horstmann <git@jhorstmann.net>
2022-12-07 16:56:12 +0100

    Sum supports results

--- src/bin/a1.rs
+++ src/bin/a1.rs
@@ -1,13 +1,21 @@
 use adventofcode2022::read_lines;
 use adventofcode2022::Result;
+use std::num::ParseIntError;
+
+type ParseIntResult<T> = std::result::Result<T, ParseIntError>;
 
 fn main() -> Result<()> {
     let lines = read_lines("data/a1.txt")?;
     let chunks = lines.split(String::is_empty).collect::<Vec<_>>();
     let mut sums = chunks
         .iter()
-        .map(|chunk| chunk.iter().map(|x: &String| x.parse::<i32>().unwrap()).sum::<i32>())
-        .collect::<Vec<_>>();
+        .map(|chunk| {
+            chunk
+                .iter()
+                .map(|x: &String| x.parse::<i32>())
+                .sum::<ParseIntResult<i32>>()
+        })
+        .collect::<ParseIntResult<Vec<i32>>>()?;
     let max = sums.iter().copied().max().unwrap_or_default();
 
     println!("part1: {max}");
