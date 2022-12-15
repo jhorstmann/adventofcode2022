84b5061fa99c1ac016e0163027d8e8ca34bb4efc
Joern Horstmann <git@jhorstmann.net>
2022-12-15 11:46:26 +0100

    Day 15 part 2

--- src/bin/a15.rs
+++ src/bin/a15.rs
@@ -1,5 +1,6 @@
 use adventofcode2022::read_lines;
 use adventofcode2022::{regex, Error, Result};
+use std::ops::{Range, RangeInclusive};
 use std::str::{from_utf8, FromStr};
 
 #[derive(Debug, Clone)]
@@ -29,6 +30,27 @@ impl Sensor {
     fn min_range(&self) -> u64 {
         self.position.0.abs_diff(self.closest_beacon.0) + self.position.1.abs_diff(self.closest_beacon.1)
     }
+
+    fn boundary_points(&self) -> impl Iterator<Item = (i64, i64)> {
+        let d = self.min_range();
+        boundary_points(self.position.0, self.position.1, d)
+    }
+
+    fn in_range(&self, x: i64, y: i64) -> bool {
+        let dist = self.position.0.abs_diff(x) + self.position.1.abs_diff(y);
+        dist <= self.min_range()
+    }
+}
+
+fn boundary_points(x: i64, y: i64, distance: u64) -> impl Iterator<Item = (i64, i64)> {
+    (0..=distance).flat_map(move |d| {
+        [
+            (x - (distance - d) as i64, y - d as i64),
+            (x - (distance - d) as i64, y + d as i64),
+            (x + (distance - d) as i64, y - d as i64),
+            (x + (distance - d) as i64, y + d as i64),
+        ]
+    })
 }
 
 pub fn main() -> Result<()> {
@@ -39,13 +61,13 @@ pub fn main() -> Result<()> {
 
     // dbg!(&sensors);
 
-    // for y in -2..20 {
-    // solve_part1(&[sensors[6].clone()], y);
-    // solve_part1(&sensors, y);
-    // }
-    println!();
-    let part1 = solve_part1(&sensors, 2000000);
+    let part1 = solve_part1(&sensors, 10);
     println!("part1: {part1}");
+
+    // dbg!(boundary_points(0, 0, 2).collect::<Vec<_>>());
+
+    let part2 = solve_part2(&sensors, 0..=4_000_000);
+    println!("part2: {part2}");
 
     Ok(())
 }
@@ -59,15 +81,12 @@ fn solve_part1(sensors: &[Sensor], ty: i64) -> usize {
 
     // dbg!(min, max);
 
-    let mut line = vec![b'.'; (max) as usize * 2];
+    let mut line = vec![b'.'; (max - min) as usize * 2];
     line.iter_mut().enumerate().for_each(|(i, b)| {
         let tx = min + (i as i64);
         let is_beacon = sensors.iter().any(|s| s.closest_beacon == (tx, ty));
         let is_sensor = sensors.iter().any(|s| s.position == (tx, ty));
-        let in_range = sensors.iter().any(|s| {
-            let dist = s.position.0.abs_diff(tx) + s.position.1.abs_diff(ty);
-            dist <= s.min_range()
-        });
+        let in_range = sensors.iter().any(|s| s.in_range(tx, ty));
         if is_beacon {
             *b = b'B';
         } else if is_sensor {
@@ -78,7 +97,20 @@ fn solve_part1(sensors: &[Sensor], ty: i64) -> usize {
     });
 
     let count = line.iter().filter(|b| **b == b'#').count();
-    println!("{:2} {} ({:3})", ty, from_utf8(&line).unwrap(), count);
+    // println!("{:2} {} ({:3})", ty, from_utf8(&line).unwrap(), count);
 
     count
+}
+
+fn solve_part2(sensors: &[Sensor], range: RangeInclusive<i64>) -> i64 {
+    let mut candidates = sensors
+        .iter()
+        .flat_map(|s| boundary_points(s.position.0, s.position.1, s.min_range() + 1))
+        .filter(|point: &(i64, i64)| range.contains(&point.0) && range.contains(&point.1))
+        .filter(|point| !sensors.iter().any(|s| s.in_range(point.0, point.1)))
+        .collect::<Vec<(i64, i64)>>();
+
+    // dbg!(&candidates);
+
+    candidates.first().map(|(x, y)| x * 4_000_000 + y).unwrap_or_default()
 }
