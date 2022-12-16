6e1ff18e160347731c7f5b201c90d414944dbeaa
Jörn Horstmann <git@jhorstmann.net>
2022-12-16 09:44:56 +0100

    Day 16 parsing

---
+++ src/bin/a16.rs
@@ -0,0 +1,89 @@
+use adventofcode2022::read_lines;
+use adventofcode2022::{regex, Error, Result};
+use std::collections::HashMap;
+use std::fmt::{Debug, Display, Formatter, Write};
+use std::str::FromStr;
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+struct Id(u16);
+
+impl Id {
+    fn try_new(s: &str) -> Result<Self> {
+        let array: [u8; 2] = s.as_bytes().try_into().map_err(|_| Error::PatternMatch)?;
+        if array[0] < b'A' || array[0] > b'Z' || array[1] < b'A' || array[1] > b'Z' {
+            Err(Error::PatternMatch)
+        } else {
+            Ok(Self(((array[0] - b'A') as u16) * 26 + (array[1] - b'A') as u16))
+        }
+    }
+    fn to_int(&self) -> usize {
+        self.0 as _
+    }
+}
+
+impl Display for Id {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_char(((self.0 / 26) as u8 + b'A') as _)?;
+        f.write_char(((self.0 % 26) as u8 + b'A') as _)?;
+        Ok(())
+    }
+}
+
+impl Debug for Id {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        <Self as Display>::fmt(self, f)
+    }
+}
+
+impl FromStr for Id {
+    type Err = Error;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        Self::try_new(s)
+    }
+}
+
+#[derive(Debug)]
+struct Valve {
+    id: Id,
+    flow_rate: u32,
+    tunnels: Vec<Id>,
+}
+
+impl FromStr for Valve {
+    type Err = Error;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let pattern = regex!(
+            "^Valve ([A-Z][A-Z]) has flow rate=([0-9]+); tunnels? leads? to valves? ([A-Z][A-Z](, [A-Z][A-Z])*)$"
+        );
+        let captures = pattern.captures(s).ok_or(Error::PatternMatch)?;
+        let id = captures[1].parse()?;
+        let flow_rate = captures[2].parse()?;
+        let tunnels = captures[3]
+            .split(", ")
+            .map(|m| m.parse())
+            .collect::<Result<Vec<Id>>>()?;
+        Ok(Self { id, flow_rate, tunnels })
+    }
+}
+
+fn solve_part1(valves: &[Valve], minutes_left: usize, current_pressure: usize) {}
+
+pub fn main() -> Result<()> {
+    let valves = read_lines("data/a16_example.txt")?
+        .iter()
+        .map(|line| Valve::from_str(line))
+        .collect::<Result<Vec<_>>>()?;
+
+    let transitions = valves
+        .iter()
+        .map(|v| (v.id, v.tunnels.clone()))
+        .collect::<HashMap<_, _>>();
+
+    let max_id = Id::try_new("ZZ").unwrap().to_int();
+
+    dbg!(max_id);
+
+    Ok(())
+}
